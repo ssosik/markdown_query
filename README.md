137f69e7b47d676f7b62feb2b04345932cc806ff
Steve <steve@little-fluffy.cloud>
2020-09-01 16:37:03 -0400

    Improving CLI handling

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # zkfm
 Minimal Zettelkasten-inspired Markdown+FrontMatter document indexer and query interface
+
+# Usage
+
+```
+# Index a source directory
+./target/debug/zkfm index ~/workspace/vimdiary
+
+# Run a query against an index
+./target/debug/zkfm query vault
+```
+
+OLD
+```
+sk --preview='bat --color=always ~/workspace/vimdiary/{}' --ansi -i -c './target/debug/tika -s ~/workspace/vimdiary -q "{}" | jq -r .filename\[0\]'
+```
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,20 @@
-use clap::Clap;
+extern crate clap;
+use clap::{Arg, App, SubCommand};
 use chrono::DateTime;
 use frontmatter;
 use glob::glob;
 use serde::{de, Deserialize, Deserializer, Serialize};
-use std::fmt;
-use std::fs;
-use std::io;
+use std::{fmt, fs, io, path::Path, env};
 use std::marker::PhantomData;
-use std::path::Path;
 use unwrap::unwrap;
 extern crate yaml_rust;
 use tantivy::collector::TopDocs;
 use tantivy::query::QueryParser;
 use tantivy::schema::*;
 use tantivy::{doc, Index, ReloadPolicy};
-use tempfile::TempDir;
+//use tempfile::TempDir;
 use yaml_rust::YamlEmitter;
-
-#[derive(Clap)]
-#[clap(version = "1.0", author = "Steve <steve@little-fluffy.cloud>")]
-struct Opts {
-    /// Set the source directory containing Markdown docs with Frontmatter
-    #[clap(short, long)]
-    source: String,
-    /// Specify the query
-    #[clap(short, long)]
-    query: String,
-    // /// Specify where to store the Index files
-    // #[clap(short, long, default_value = "")]
-    // index_path: String,
-}
+extern crate shellexpand;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Doc {
@@ -78,16 +63,38 @@ where
 fn main() -> tantivy::Result<()> {
     color_backtrace::install();
 
-    let opts: Opts = Opts::parse();
+    let default_index_dir = shellexpand::tilde("~/.config/zkfm/");
 
-    ///let index_path = TempDir::new()?;
-    //let index_path = match opts.index_path {
-    //    "" => {
-    //        let t = TempDir::new()?;
-    //        t.path()
-    //    },
-    //    _ => opts.index_path,
-    //};
+    let matches = App::new("zkfm")
+                      .version("1.0")
+                      .author("Steve <steve@little-fluffy.cloud>")
+                      .about("Zettlekasten-inspired Markdown+FrontMatter Indexer and query tool")
+                      .arg(Arg::with_name("index_path")
+                           .short("i")
+                           .value_name("DIRECTORY")
+                           .help("Set the directory to store Tantivy index data")
+                           .default_value(&default_index_dir)
+                           .takes_value(true))
+                      .arg(Arg::with_name("source")
+                           .short("s")
+                           .long("source")
+                           .value_name("DIRECTORY")
+                           .help("Set the source directory containing Markdown docs with Frontmatter")
+                           .takes_value(true))
+                      .subcommand(SubCommand::with_name("index")
+                                  .about("Load data from a source directory")
+                                  .arg(Arg::with_name("source")
+                                      .help("print debug information verbosely")))
+                      .subcommand(SubCommand::with_name("query")
+                                  .about("Query the index")
+                                  .arg(Arg::with_name("query")
+                                      .help("print debug information verbosely")))
+                      .get_matches();
+
+    let index_path = matches.value_of("index_path").unwrap();
+
+    let index_path = Path::new(&index_path);
+    fs::create_dir_all(index_path)?;
 
     let mut schema_builder = Schema::builder();
     let author = schema_builder.add_text_field("author", TEXT);
@@ -99,64 +106,71 @@ fn main() -> tantivy::Result<()> {
 
     let schema = schema_builder.build();
 
-    //let index = Index::create_in_dir(Path::new("indexdata"), schema.clone()).unwrap();
-    let d = tantivy::directory::MmapDirectory::open(Path::new("indexdata")).unwrap();
+    let d = tantivy::directory::MmapDirectory::open(index_path).unwrap();
     let index = Index::open_or_create(d, schema.clone()).unwrap();
 
-    let mut index_writer = index.writer(100_000_000).unwrap();
+    if let Some(matches) = matches.subcommand_matches("index") {
+        let source = matches.value_of("source").unwrap();
 
-    let glob_path = Path::new(&opts.source).join("*.md");
-    let glob_str = glob_path.to_str().unwrap();
+        let mut index_writer = index.writer(100_000_000).unwrap();
 
-    //println!("Directory: {}", glob_str);
+        let glob_path = Path::new(&source).join("*.md");
+        let glob_str = glob_path.to_str().unwrap();
 
-    //for entry in glob(glob_str).expect("Failed to read glob pattern") {
-    //    match entry {
-    //        Ok(path) => {
-    //            println!("Processing {:?}", path.display());
-    //            let res = index_file(&path);
-    //            let doc = unwrap!(res, "Failed to process file {}", path.display());
-    //            let rfc3339 = DateTime::parse_from_rfc3339(&doc.date).unwrap();
-    //            let thingit = rfc3339.with_timezone(&chrono::Utc);
-    //            let thedate = Value::Date(thingit);
+        println!("Directory: {}", glob_str);
 
-    //            index_writer.add_document(doc!(
-    //                author => doc.author,
-    //                body => doc.body,
-    //                date => thedate,
-    //                filename => doc.filename,
-    //                tags => doc.tags.join(" "),
-    //                title => doc.title,
-    //            ));
-    //        }
-    //        Err(e) => println!("{:?}", e),
-    //    }
-    //}
+        for entry in glob(glob_str).expect("Failed to read glob pattern") {
+            match entry {
+                Ok(path) => {
+                    println!("Processing {:?}", path.display());
+                    let res = index_file(&path);
+                    let doc = unwrap!(res, "Failed to process file {}", path.display());
+                    let rfc3339 = DateTime::parse_from_rfc3339(&doc.date).unwrap();
+                    let thingit = rfc3339.with_timezone(&chrono::Utc);
+                    let thedate = Value::Date(thingit);
 
-    //index_writer.commit().unwrap();
+                    index_writer.add_document(doc!(
+                        author => doc.author,
+                        body => doc.body,
+                        date => thedate,
+                        filename => doc.filename,
+                        tags => doc.tags.join(" "),
+                        title => doc.title,
+                    ));
+                }
+                Err(e) => println!("{:?}", e),
+            }
+        }
 
-    // TODO replace with meaningful queries
-    let reader = index
-        .reader_builder()
-        .reload_policy(ReloadPolicy::OnCommit)
-        .try_into()?;
+        index_writer.commit().unwrap();
+    }
 
-    let searcher = reader.searcher();
+    if let Some(matches) = matches.subcommand_matches("query") {
+        let query = matches.value_of("query").unwrap();
 
-    let query_parser = QueryParser::for_index(&index, vec![author, body, filename, tags, title]);
+        // TODO replace with meaningful queries
+        let reader = index
+            .reader_builder()
+            .reload_policy(ReloadPolicy::OnCommit)
+            .try_into()?;
 
-    //let query = query_parser.parse_query("vim")?;
-    //let query = query_parser.parse_query("tags:kubernetes")?;
-    //let query = query_parser.parse_query("date:2020-07-24T13:03:50-04:00")?;
-    //let query = query_parser.parse_query("* AND date:\"2019-04-01T14:02:03Z\"")?;
-    //let query = query_parser.parse_query("* AND NOT date:\"2019-04-01T14:02:03Z\"")?;
-    let query = query_parser.parse_query(&opts.query)?;
+        let searcher = reader.searcher();
 
-    let top_docs = searcher.search(&query, &TopDocs::with_limit(100))?;
+        let query_parser = QueryParser::for_index(&index, vec![author, body, filename, tags, title]);
 
-    for (_score, doc_address) in top_docs {
-        let retrieved_doc = searcher.doc(doc_address)?;
-        println!("{}", schema.to_json(&retrieved_doc));
+        //let query = query_parser.parse_query("vim")?;
+        //let query = query_parser.parse_query("tags:kubernetes")?;
+        //let query = query_parser.parse_query("date:2020-07-24T13:03:50-04:00")?;
+        //let query = query_parser.parse_query("* AND date:\"2019-04-01T14:02:03Z\"")?;
+        //let query = query_parser.parse_query("* AND NOT date:\"2019-04-01T14:02:03Z\"")?;
+        let query = query_parser.parse_query(&query)?;
+
+        let top_docs = searcher.search(&query, &TopDocs::with_limit(100))?;
+
+        for (_score, doc_address) in top_docs {
+            let retrieved_doc = searcher.doc(doc_address)?;
+            println!("{}", schema.to_json(&retrieved_doc));
+        }
     }
 
     //println!("index_path: {:?}", index_path);
