0bea3eb39add54c502af104b435bc054132e88ed
steve <steve@little-fluffy.cloud>
2022-03-19 14:16:42 -0400

    Switch from TermionBackend back to CrosstermBackend

--- src/interactive.rs
+++ src/interactive.rs
@@ -12,7 +12,7 @@ use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
 use tempfile::Builder;
 use termion::{event::Key, raw::IntoRawMode, screen::AlternateScreen};
 use tui::{
-    backend::TermionBackend,
+    backend::CrosstermBackend,
     layout::{Constraint, Direction, Layout},
     style::{Color, Modifier, Style},
     text::{Span, Spans, Text},
@@ -127,7 +127,7 @@ pub fn query(
     pager: String,
     editor: String,
 ) -> Result<Vec<String>, Report> {
-    let mut tui = tui::Terminal::new(TermionBackend::new(AlternateScreen::from(
+    let mut tui = tui::Terminal::new(CrosstermBackend::new(AlternateScreen::from(
         stdout().into_raw_mode().unwrap(),
     )))
     .unwrap();
@@ -368,7 +368,7 @@ pub fn query(
                                 .status()
                                 .expect("failed to execute process");
                             events = event::Events::new();
-                            tui = tui::Terminal::new(TermionBackend::new(AlternateScreen::from(
+                            tui = tui::Terminal::new(CrosstermBackend::new(AlternateScreen::from(
                                 stdout().into_raw_mode().unwrap(),
                             )))
                             .unwrap();
@@ -399,7 +399,7 @@ pub fn query(
                                 .status()
                                 .expect("failed to execute process");
                             events = event::Events::new();
-                            tui = tui::Terminal::new(TermionBackend::new(AlternateScreen::from(
+                            tui = tui::Terminal::new(CrosstermBackend::new(AlternateScreen::from(
                                 stdout().into_raw_mode().unwrap(),
                             )))
                             .unwrap();
