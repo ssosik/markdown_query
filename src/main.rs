4fd15bc749352dd4ab290276021dc917522888a6
steve <steve@little-fluffy.cloud>
2022-03-23 10:18:57 -0400

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -154,18 +154,15 @@ fn main() -> Result<(), Report> {
                         .on_pre_event_inner(Key::PageUp, |v, _| {
                             let scroller = v.get_scroller_mut();
                             if scroller.can_scroll_up() {
-                                scroller.scroll_up(
-                                    scroller.last_outer_size().y.saturating_sub(1),
-                                );
+                                scroller.scroll_up(scroller.last_outer_size().y.saturating_sub(1));
                             }
                             Some(EventResult::Consumed(None))
                         })
                         .on_pre_event_inner(Key::PageDown, |v, _| {
                             let scroller = v.get_scroller_mut();
                             if scroller.can_scroll_down() {
-                                scroller.scroll_down(
-                                    scroller.last_outer_size().y.saturating_sub(1),
-                                );
+                                scroller
+                                    .scroll_down(scroller.last_outer_size().y.saturating_sub(1));
                             }
                             Some(EventResult::Consumed(None))
                         }),
